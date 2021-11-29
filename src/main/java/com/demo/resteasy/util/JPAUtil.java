2bb32d6b2280a2a1301e068c83c97be2a6592ebb
Serendipity <jacksparrow414@163.com>
2021-11-29 09:39:49 +0800

    bootstrap JPA

---
+++ src/main/java/com/demo/resteasy/util/JPAUtil.java
@@ -0,0 +1,45 @@
+package com.demo.resteasy.util;
+
+import lombok.extern.java.Log;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.logging.Level;
+
+/**
+ * Tomcat不会引导JPA，所以需要手动引导JPA
+ */
+@Log
+public class JPAUtil {
+
+    private static final String PERSISTENCE_UNIT_NAME = "restEasy";
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    public static final ThreadLocal<EntityManager> threadLocal = new ThreadLocal<>();
+
+    /**
+     * 引导JPA
+     */
+    public static void bootstrapJPA() {
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Create entity manager factory failure for persistence unit " + PERSISTENCE_UNIT_NAME, e);
+        }
+    }
+
+    /**
+     * EntityManager
+     * @return
+     */
+    public static EntityManager getEntityManager(){
+        EntityManager manager = threadLocal.get();
+        if(manager == null || !manager.isOpen()){
+            manager = entityManagerFactory.createEntityManager();
+            threadLocal.set(manager);
+        }
+        return manager;
+    }
+}
--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,6 +1,3 @@
-INSERT INTO `user` (`user_id`, `pass_word`, `roles`, `scopes`)
-VALUES ('appuser', 'appusersecret', 'USER', 'resource.read resource.write');
+INSERT INTO `user` (`user_id`, `pass_word`, `roles`, `scopes`)VALUES ('appuser', 'appusersecret', 'USER', 'resource.read resource.write');
 
-INSERT INTO `client` (`client_id`, `client_secret`, `redirect_url`, `scopes`, `authorized_grant_types`)
-VALUES ('webappclient', 'webappclientsecret', 'http://localhost:9180/callback', 'resource.read resource.write',
-        'authorization_code refresh_token');
+INSERT INTO `client` (`client_id`, `client_secret`, `redirect_url`, `scopes`, `authorized_grant_types`)VALUES ('webappclient', 'webappclientsecret', 'http://localhost:9180/callback', 'resource.read resource.write','authorization_code refresh_token');
