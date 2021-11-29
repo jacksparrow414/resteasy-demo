7b368bc35f64fe59925ce0bd9dc829635f1148fc
Serendipity <jacksparrow414@163.com>
2021-11-29 09:39:49 +0800

    bootstrap JPA

--- src/main/java/com/demo/resteasy/WebAppListener.java
+++ src/main/java/com/demo/resteasy/WebAppListener.java
@@ -1,37 +1,39 @@
 package com.demo.resteasy;
 
-import java.sql.SQLException;
+import com.demo.resteasy.util.JPAUtil;
+import lombok.extern.java.Log;
+import org.h2.tools.Server;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.sound.midi.Soundbank;
 import javax.sql.DataSource;
+import java.util.logging.Level;
 
 /**
  * @author jacksparrow414
  * @date 2021/11/27
+ * 用来测试JNDI数据源是否配置成功，以及JPA是否启动成功
+ * {@link com.demo.resteasy.util.JPAUtil}
  */
+@Log
 public class WebAppListener implements ServletContextListener {
     
     @Override
     public void contextInitialized(ServletContextEvent event) {
-        try
-        {
+        try {
             Context initCtx = new InitialContext();
             Context envCtx = (Context) initCtx.lookup("java:comp/env");
             DataSource ds = (DataSource) envCtx.lookup("jdbc/myLocalJPADB");
-            EntityManagerFactory restEasy = Persistence.createEntityManagerFactory("restEasy");
-            System.out.println("success");
+            JPAUtil.bootstrapJPA();
+            log.info("Bootstrap JPA Success");
         } catch (NamingException ex) {
-            System.out.println("!!!! Got NamingException:");
-            ex.printStackTrace(System.out);
+            log.log(Level.WARNING,"!!!! Got NamingException:", ex);
         }
     }
-    
+
     @Override
     public void contextDestroyed(ServletContextEvent event) {
     }
